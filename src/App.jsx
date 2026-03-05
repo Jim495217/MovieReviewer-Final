0d771699ecaab2562ff3d581a88288acb7918d42
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2026-03-05 12:14:39 -0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import MoviePage from "./pages/MoviePage";
-import SearchPage from "./pages/SearchPage";
-import FavoritesPage from "./pages/FavoritesPage";
-import Navbar from "./components/Navbar";
-import NotFound from "./components/NotFound";
-
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/movie/:id" element={<MoviePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+  return <h1>Movie Reviewer Working</h1>;
 }
 
 export default App;
